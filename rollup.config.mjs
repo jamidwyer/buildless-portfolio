98191f620678c86b674ce97ca5fe6b26110695bf
Jami Dwyer <jamidwyer@yahoo.com>
2024-06-03 23:21:45 -0700

    Load JSON

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from "@rollup/plugin-json";
 
 export default {
   input: 'index.html',
   output: { dir: 'dist' },
   plugins: [
     html(),
+    json(),
     nodeResolve()
 ],
 };
