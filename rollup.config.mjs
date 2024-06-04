5d00d70951a168aaa6eccdf84d839e786139b5a1
Jami Dwyer <jamidwyer@yahoo.com>
2024-06-03 23:32:39 -0700

    Copy assets

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from "@rollup/plugin-json";
+import { copy } from '@web/rollup-plugin-copy';
 
 export default {
   input: 'index.html',
   output: { dir: 'dist' },
   plugins: [
+    copy({ patterns: '**/*.{svg,jpg,png,jpeg,json}' }),
     html(),
     json(),
     nodeResolve()
